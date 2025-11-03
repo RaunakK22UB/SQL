295ef9ce3c0542c2f2f0afd31d53a621b9dbe9cf
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-11-03 22:53:32 +0530

    177_leetcode

--- 177_leetcode.sql
+++ 177_leetcode.sql
@@ -3,4 +3,6 @@ BEGIN
 set N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-        
+        select distinct salary from employee order by salary desc limit 1 offset N
+  );
+END
